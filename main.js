75c4b095deb3df81f9ba5effa8453e4fc35c9f03
Wizard-lab-creator <80611597+Wizard-lab-creator@users.noreply.github.com>
2021-12-02 15:44:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(400, 400);
+    video.position(10, 50)
+    canvas = createCanvas(400, 400);
+    canvas.position(430, );
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet is Initialized")
+}
+function draw(){
+    background("#5196e3");
+}
+function gotPoses(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
+}
